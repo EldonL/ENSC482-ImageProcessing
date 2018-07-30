8cfa0aaee8bc2d74809ef090a3b85e7501eb65d6
EldonL <eldonl@sfu.ca>
2018-07-30 15:53:18 -0700

    Add tracking codes
    Codes have been reorganized
    /*INSTRUCTION: Comment out either "JC and Camera Codes" (4 lines)  or TrackBarCodes (1 line)*/

--- Source_Header_Files/Main.cpp
+++ Source_Header_Files/Main.cpp
@@ -1,4 +1,4 @@
-/**
+/*
 //Source.cpp
 //Created by: Eldon Lin
 //Last Edited by: Eldon Lin
@@ -17,7 +17,8 @@
 // "Ball Tracking / Detection using OpenCV " http://anikettatipamula.blogspot.com/2012/12/ball-tracking-detection-using-opencv.html [Accessed July 30, 2018]
 // "How to Detect and Track Object With OpenCV" https://www.intorobotics.com/how-to-detect-and-track-object-with-opencv/ [Accessed July 19,2018]
 // "OpenCV Basics - 12 - Webcam & Video Capture" https://www.youtube.com/watch?v=zhEqiW3qnos [Accessed: July 30,2018]
-//*/
+H. Kyle "Tutorial: Real-Time Object Tracking Using OpenCV" https://www.youtube.com/watch?v=bSeFrPrqZ2A [Accessed July 30,2018]
+*/
 
 
 /*
@@ -53,6 +54,8 @@ References:
 using namespace cv;
 using namespace std;
 
+#pragma region JC codes and Camera link 
+
 Mat src, erosion_dst, dilation_dst;
 bool isCircle = false; 
 /**
@@ -86,16 +89,8 @@ void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& cont
 	cv::putText(im, label, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
 }
 
-int main(int argc, char** argv)
+void FindShapeContours(Mat src)
 {
-	CommandLineParser parser(argc, argv, "{@input | ../data/lena.jpg | input image}");
-	src = imread(parser.get<String>("@input"),IMREAD_COLOR);
-	/*if (src.empty())
-		return -1;*/
-	/*Filter filter;
-	filter.smoothingByBilateral(src);*/
-	
-
 	// Convert to grayscale
 	cv::Mat gray;
 	cv::cvtColor(src, gray, CV_BGR2GRAY);
@@ -111,7 +106,7 @@ int main(int argc, char** argv)
 	//Original code was 5. Changed to 7
 	cv::Mat bw;
 	cv::Canny(gray, bw, 0, 500, 7);
-	
+
 	// Find contours
 	std::vector<std::vector<cv::Point> > contours;
 	cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
@@ -159,13 +154,13 @@ int main(int argc, char** argv)
 			//else if (vtc == 6 && mincos >= -0.55 && maxcos <= -0.45)
 			else if (vtc == 6 /*&& mincos >= -0.55 && maxcos <= -0.45*/)
 				setLabel(dst, "HEXA", contours[i]);
-			else if (vtc == 7 )
+			else if (vtc == 7)
 				setLabel(dst, "hepta", contours[i]);
 			/*else if (vtc == 8 && mincos >= -0.55 && maxcos <= -0.45)
-				setLabel(dst, "Octa", contours[i]);*/
+			setLabel(dst, "Octa", contours[i]);*/
 			else
 				setLabel(dst, "unknown", contours[i]);
-		
+
 		}
 		else
 		{
@@ -198,12 +193,12 @@ int main(int argc, char** argv)
 				//circle(dst, center, 3, Scalar(0, 255, 0), -1, 8, 0);
 				// circle outline
 				circle(dst, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-				isCircle = true; 
+				isCircle = true;
 			}
 			if (isCircle)
 			{
 				setLabel(dst, "CIR", contours[i]);
-				isCircle = false; 
+				isCircle = false;
 			}
 			else if (vtc == 8)
 			{
@@ -215,15 +210,15 @@ int main(int argc, char** argv)
 			else
 				setLabel(dst, "round obj", contours[i]);
 			/*if (vtc == 8 && mincos >= -0.75 && maxcos <= -0.7)
-				setLabel(dst, "Octagon", contours[i]);
+			setLabel(dst, "Octagon", contours[i]);
 			else if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
-				std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
-				setLabel(dst, "CIR", contours[i]);
+			std::abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
+			setLabel(dst, "CIR", contours[i]);
 			else if (std::abs(1 - ((double)r.width / r.height)) >= 0.2 &&
-				std::abs(1 - (area / (CV_PI * (double)r.width * (double)r.height)) >= 0.2))
-				setLabel(dst, "OVL", contours[i]);
+			std::abs(1 - (area / (CV_PI * (double)r.width * (double)r.height)) >= 0.2))
+			setLabel(dst, "OVL", contours[i]);
 			else
-				setLabel(dst, "round obj", contours[i]);*/
+			setLabel(dst, "round obj", contours[i]);*/
 		}
 	}
 	//cv::imshow("gray", gray);
@@ -231,7 +226,10 @@ int main(int argc, char** argv)
 	//cv::imshow("src", src);
 	cv::imshow("dst", dst);
 	//cv::waitKey(0);
+}
 
+int CameraLink()
+{
 	//Video stuff
 	VideoCapture cap(0); // open the default camera
 	if (!cap.isOpened())  // check if we succeeded
@@ -253,92 +251,257 @@ int main(int argc, char** argv)
 	return 0;
 
 }
+#pragma endregion 
 
-//
-//#include "opencv2/imgproc.hpp"
-//#include "opencv2/highgui.hpp"
-//#include <iostream>
-//
-//using namespace cv;
-//using namespace std;
-//
-//
-//Mat src, erosion_dst, dilation_dst;
-//
-//
-//
-//#include "Filter.h"
-//
-//
-//#pragma region main
-//int main(int argc, char** argv)
-//{
-//	//The first four lines below is how you would call an image. The output would show the before image and the binarized image
-//	
-//	/// Load an image
-//	CommandLineParser parser(argc, argv, "{@input | ../data/lena.jpg | input image}");
-//	src = imread(parser.get<String>("@input"),IMREAD_COLOR);
-//	Filter filter; 
-//	filter.smoothingByBilateral(src);
-//	
-//
-//	system("pause");
-//	
-//
-//}
-//#pragma endregion
-//
-////for (int i = 0; i < contours.size(); i++)
-////{
-////	// Approximate contour with accuracy proportional
-////	// to the contour perimeter
-////	cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
-////
-////	// Skip small or non-convex objects
-////	if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
-////		continue;
-////
-////	if (approx.size() == 3)
-////	{
-////		setLabel(dst, "TRI", contours[i]);    // Triangles
-////	}
-////	else if (approx.size() >= 4 && approx.size() <= 6)
-////	{
-////		// Number of vertices of polygonal curve
-////		int vtc = approx.size();
-////
-////		// Get the cosines of all corners
-////		std::vector cos;
-////		for (int j = 2; j < vtc + 1; j++)
-////			cos.push_back(angle(approx[j%vtc], approx[j - 2], approx[j - 1]));
-////
-////		// Sort ascending the cosine values
-////		std::sort(cos.begin(), cos.end());
-////
-////		// Get the lowest and the highest cosine
-////		double mincos = cos.front();
-////		double maxcos = cos.back();
-////
-////		// Use the degrees obtained above and the number of vertices
-////		// to determine the shape of the contour
-////		if (vtc == 4)
-////			setLabel(dst, "RECT", contours[i]);
-////		else if (vtc == 5)
-////			setLabel(dst, "PENTA", contours[i]);
-////		else if (vtc == 6)
-////			setLabel(dst, "HEXA", contours[i]);
-////	}
-////	else
-////	{
-////		// Detect and label circles
-////		double area = cv::contourArea(contours[i]);
-////		cv::Rect r = cv::boundingRect(contours[i]);
-////		int radius = r.width / 2;
-////
-////		if (std::abs(1 - ((double)r.width / r.height)) <= 0.2 &&
-////			std::abs(1 - (area / (CV_PI * (radius*radius)))) <= 0.2)
-////			setLabel(dst, "CIR", contours[i]);
-////	}
-////}
-////cv::imshow("src", src);
-////cv::imshow("dst", dst);
+
+#pragma region TrackBarCodes from online
+
+
+
+//objectTrackingTutorial.cpp
+
+//Written by  Kyle Hounslow 2013
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software")
+//, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//IN THE SOFTWARE.
+
+
+//initial min and max HSV filter values.
+//these will be changed using trackbars
+int H_MIN = 0;
+int H_MAX = 256;
+int S_MIN = 0;
+int S_MAX = 256;
+int V_MIN = 0;
+int V_MAX = 256;
+//default capture width and height
+const int FRAME_WIDTH = 640;
+const int FRAME_HEIGHT = 480;
+//max number of objects to be detected in frame
+const int MAX_NUM_OBJECTS = 50;
+//minimum and maximum object area
+const int MIN_OBJECT_AREA = 20 * 20;
+const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH / 1.5;
+//names that will appear at the top of each window
+const string windowName = "Original Image";
+const string windowName1 = "HSV Image";
+const string windowName2 = "Thresholded Image";
+const string windowName3 = "After Morphological Operations";
+const string trackbarWindowName = "Trackbars";
+void on_trackbar(int, void*)
+{//This function gets called whenever a
+ // trackbar position is changed
+
+
+
+
+
+}
+string intToString(int number) {
+
+
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+void createTrackbars() {
+	//create window for trackbars
+
+
+	namedWindow(trackbarWindowName, 0);
+	//create memory to store trackbar name on window
+	char TrackbarName[50];
+	sprintf(TrackbarName, "H_MIN", H_MIN);
+	sprintf(TrackbarName, "H_MAX", H_MAX);
+	sprintf(TrackbarName, "S_MIN", S_MIN);
+	sprintf(TrackbarName, "S_MAX", S_MAX);
+	sprintf(TrackbarName, "V_MIN", V_MIN);
+	sprintf(TrackbarName, "V_MAX", V_MAX);
+	//create trackbars and insert them into window
+	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
+	//the max value the trackbar can move (eg. H_HIGH), 
+	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
+	//                                  ---->    ---->     ---->      
+	createTrackbar("H_MIN", trackbarWindowName, &H_MIN, H_MAX, on_trackbar);
+	createTrackbar("H_MAX", trackbarWindowName, &H_MAX, H_MAX, on_trackbar);
+	createTrackbar("S_MIN", trackbarWindowName, &S_MIN, S_MAX, on_trackbar);
+	createTrackbar("S_MAX", trackbarWindowName, &S_MAX, S_MAX, on_trackbar);
+	createTrackbar("V_MIN", trackbarWindowName, &V_MIN, V_MAX, on_trackbar);
+	createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
+
+
+}
+void drawObject(int x, int y, Mat &frame) {
+
+	//use some of the openCV drawing functions to draw crosshairs
+	//on your tracked image!
+
+	//UPDATE:JUNE 18TH, 2013
+	//added 'if' and 'else' statements to prevent
+	//memory errors from writing off the screen (ie. (-25,-25) is not within the window!)
+
+	circle(frame, Point(x, y), 20, Scalar(0, 255, 0), 2);
+	if (y - 25>0)
+		line(frame, Point(x, y), Point(x, y - 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, 0), Scalar(0, 255, 0), 2);
+	if (y + 25<FRAME_HEIGHT)
+		line(frame, Point(x, y), Point(x, y + 25), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(x, FRAME_HEIGHT), Scalar(0, 255, 0), 2);
+	if (x - 25>0)
+		line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
+	if (x + 25<FRAME_WIDTH)
+		line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
+	else line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
+
+	putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
+
+}
+void morphOps(Mat &thresh) {
+
+	//create structuring element that will be used to "dilate" and "erode" image.
+	//the element chosen here is a 3px by 3px rectangle
+
+	Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3, 3));
+	//dilate with larger element so make sure object is nicely visible
+	Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8, 8));
+
+	erode(thresh, thresh, erodeElement);
+	erode(thresh, thresh, erodeElement);
+
+
+	dilate(thresh, thresh, dilateElement);
+	dilate(thresh, thresh, dilateElement);
+
+
+
+}
+void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed) {
+
+	Mat temp;
+	threshold.copyTo(temp);
+	//these two vectors needed for output of findContours
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	//find contours of filtered image using openCV findContours function
+	findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if (numObjects<MAX_NUM_OBJECTS) {
+			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+
+				//if the area is less than 20 px by 20px then it is probably just noise
+				//if the area is the same as the 3/2 of the image size, probably just a bad filter
+				//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if (area>MIN_OBJECT_AREA && area<MAX_OBJECT_AREA && area>refArea) {
+					x = moment.m10 / area;
+					y = moment.m01 / area;
+					objectFound = true;
+					refArea = area;
+				}
+				else objectFound = false;
+
+
+			}
+			//let user know you found an object
+			if (objectFound == true) {
+				putText(cameraFeed, "Tracking Object", Point(0, 50), 2, 1, Scalar(0, 255, 0), 2);
+				//draw object location on screen
+				drawObject(x, y, cameraFeed);
+			}
+
+		}
+		else putText(cameraFeed, "TOO MUCH NOISE! ADJUST FILTER", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+	}
+}
+
+void TrackBarCodes()
+{
+	//some boolean variables for different functionality within this
+	//program
+	bool trackObjects = true;
+	bool useMorphOps = true;
+	//Matrix to store each frame of the webcam feed
+	Mat cameraFeed;
+	//matrix storage for HSV image
+	Mat HSV;
+	//matrix storage for binary threshold image
+	Mat threshold;
+	//x and y values for the location of the object
+	int x = 0, y = 0;
+	//create slider bars for HSV filtering
+	createTrackbars();
+	//video capture object to acquire webcam feed
+	VideoCapture capture;
+	//open capture object at location zero (default location for webcam)
+	capture.open(0);
+	//set height and width of capture frame
+	capture.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
+	capture.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
+	//start an infinite loop where webcam feed is copied to cameraFeed matrix
+	//all of our operations will be performed within this loop
+	while (1) {
+		//store image to matrix
+		capture.read(cameraFeed);
+		//convert frame from BGR to HSV colorspace
+		cvtColor(cameraFeed, HSV, COLOR_BGR2HSV);
+		//filter HSV image between values and store filtered image to
+		//threshold matrix
+		inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
+		//perform morphological operations on thresholded image to eliminate noise
+		//and emphasize the filtered object(s)
+		if (useMorphOps)
+			morphOps(threshold);
+		//pass in thresholded frame to our object tracking function
+		//this function will return the x and y coordinates of the
+		//filtered object
+		if (trackObjects)
+			trackFilteredObject(x, y, threshold, cameraFeed);
+
+		//show frames 
+		imshow(windowName2, threshold);
+		imshow(windowName, cameraFeed);
+		imshow(windowName1, HSV);
+
+
+		//delay 30ms so that screen can refresh.
+		//image will not appear without this waitKey() command
+		waitKey(30);
+	}
+}
+#pragma endregion
+
+int main(int argc, char* argv[])
+{
+
+	/*INSTRUCTION: Comment out either "JC and Camera Codes" (4 lines)  or TrackBarCodes (1 line)*/
+	
+	//JC and Camera Codes
+	CommandLineParser parser(argc, argv, "{@input | ../data/lena.jpg | input image}");
+	src = imread(parser.get<String>("@input"), IMREAD_COLOR);
+	FindShapeContours(src);
+	CameraLink();
+	
+	//Online Codes
+	//Converts RGB video to HSV. Tracks a specific object. Found on https://www.youtube.com/watch?v=bSeFrPrqZ2A
+	//TrackBarCodes();
+
+	return 0;
+}
+
